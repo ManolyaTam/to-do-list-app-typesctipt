dd8b0b3cede18cb3362622b335bca772e43fb1e3
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-12 09:51:29 +0300

    moving fetch service to useTodoList hook

--- src/hooks/useTodoList.tsx
+++ src/hooks/useTodoList.tsx
@@ -1,6 +1,23 @@
 import { useState, useEffect } from "react";
-import { fetchTodos } from '../services/CRUD';
 import { Todo } from '../types/todo';
+
+const fetchTodos = async (): Promise<Todo[] | null> => {
+  return fetch('https://dummyjson.com/todos')
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+          .then(json => json.todos);
+      }
+      else {
+        console.log('unexpected response status code', res.status, res.statusText)
+        return null;
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      return null;
+    });
+}
 
 const useTodoList = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
